517aa7fa1768b4bca1a656a38b031f191bccba29
pradeep-chandra <34370619+pradeep-chandra@users.noreply.github.com>
2017-12-08 22:08:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from flask import make_response
 app = Flask(__name__)
 
 
-@app.route('/webhook', methods=['POST'])
+@app.route('/hackathonapplication', methods=['POST'])
 def webhook():
     req = request.get_json(silent=True, force=True)
 
